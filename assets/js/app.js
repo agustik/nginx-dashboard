74c37eb00b9010ed8a376c910e346d0afedd04be
Agust Ingi kjartansson <agustik@lsh.is>
2015-06-25 21:38:35 +0000

    added graphs and more

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,38 @@
+
+"use strict"
+
 var app = angular.module('nginx-dashboard', ['gridshore.c3js.chart', 'ui.bootstrap']);
 
-console.log(config.server);
-var graphApp = angular.module('graphApp', ['gridshore.c3js.chart']);
-graphApp.controller('GraphCtrl', function ($scope) {
-});
 
 
-app.controller('main', [ "$scope", "$interval", "req", function ($scope, $interval, req){
+
+app.controller('main', [ "$scope", "$interval", "$log", "req", function ($scope, $interval, $log, req){
+
+	$log.info(config.server);
 	$scope.nginx= {};
 
-	$scope.upstreamState=[];
+	$scope.mainGraph = [];
+	$scope.mainTrafficGraph = [];
 
+	$scope.mainTrafficGraphCols=[
+			{"id":"Received","type":"spline"},
+      {"id":"Sent","type":"spline"}
+	];
+
+	$scope.mainGraphCols=[
+		{"id":"Requests","type":"spline"},
+		{"id":"2xx","type":"area"},
+		{"id":"3xx","type":"area"},
+		{"id":"4xx","type":"area"},
+		{"id":"5xx","type":"area"}
+		];
+
+	$scope.ServerGraphsTraffic=[{"id":"Received","type":"spline"},
+                        {"id":"Sent","type":"spline"}];
+
+	$scope.ServerGraphsStatus=[{"id":"2xx","type":"spline"},
+                        {"id":"4xx-5xx","type":"spline"}];
+	$scope.datax={"id":"x"};
 	$scope.status ={
 			server: {
 				state : true,
@@ -29,18 +51,12 @@ app.controller('main', [ "$scope", "$interval", "req", function ($scope, $interv
 	$scope.cacheAdvanced=false;
 
 	$scope.start = $interval(function (){
-
 		req.get().then(function (d){
-
+			var now = new Date().getTime(), statusBad_S, statusGood_S;
 			d.uptime = d.nowMsec - d.loadMsec;
-
 			$scope.upstreamState=$scope.getHostState(d);
 
 			d.connections.total = $scope.getTotal(d.connections);
-
-			if('connections' in $scope.nginx){
-				d.connections.reqs = $scope.prSecond(d.connections.total, $scope.nginx.connections.total);
-			}
 
 			if ('upstreamZones' in $scope.nginx){
 				for (var upstream in d.upstreamZones){
@@ -53,20 +69,25 @@ app.controller('main', [ "$scope", "$interval", "req", function ($scope, $interv
 			}
 
 			for (var zone in d.serverZones){
-
-
-				d.serverZones[zone].responses.statusTotal=$scope.getTotal(d.serverZones[zone].responses, 'status');
-				d.serverZones[zone].responses.cacheTotal=$scope.getTotal(d.serverZones[zone].responses, 'cache');
-
 				if('serverZones' in $scope.nginx){
 
 					d.serverZones[zone].requestCounter_S=$scope.prSecond(d.serverZones[zone].requestCounter, $scope.nginx.serverZones[zone].requestCounter);
 
 					d.serverZones[zone].inBytes_S=$scope.prSecond(d.serverZones[zone].inBytes, $scope.nginx.serverZones[zone].inBytes);
-					d.serverZones[zone].outBytes_S=$scope.prSecond(d.serverZones[zone].outBytes, $scope.nginx.serverZones[zone].outBytes)
+					d.serverZones[zone].outBytes_S=$scope.prSecond(d.serverZones[zone].outBytes, $scope.nginx.serverZones[zone].outBytes);
 
+					d.serverZones[zone].totalgoodstatus = $scope.RegexCount(d.serverZones[zone].responses, '[1-3]xx');
+					d.serverZones[zone].totalbadstatus = $scope.RegexCount(d.serverZones[zone].responses, '[4-5]xx');
+
+					d.serverZones[zone].statusTotal=$scope.getTotal(d.serverZones[zone].responses, 'status');
+					d.serverZones[zone].cacheTotal=$scope.getTotal(d.serverZones[zone].responses, 'cache');
+
+
+				}else{
+					$scope.ServerTraffic[zone] = [];
+					$scope.ServerStatus[zone] = [];
 				}
-				
+
 				d.serverZones[zone].hitRatio=$scope.hitRatio(d.serverZones[zone].responses);
 
 			}
@@ -77,16 +98,50 @@ app.controller('main', [ "$scope", "$interval", "req", function ($scope, $interv
 					d.cacheZones[zone].inBytes_S=$scope.prSecond(d.cacheZones[zone].inBytes, $scope.nginx.cacheZones[zone].inBytes);
 					d.cacheZones[zone].outBytes_S=$scope.prSecond(d.cacheZones[zone].outBytes, $scope.nginx.cacheZones[zone].outBytes);
 				}
-				d.cacheZones[zone].responses.total=$scope.getTotal(d.cacheZones[zone].responses, 'cache');
+				d.cacheZones[zone].total=$scope.getTotal(d.cacheZones[zone].responses, 'cache');
 				d.cacheZones[zone].hitRatio=$scope.hitRatio(d.cacheZones[zone].responses);
 			}
+			d.TotalErrorCodes = $scope.aggrigateErrorCodes(d.serverZones);
 
-			console.log(d);
+			d.traffic = $scope.aggrigateTraffic(d.serverZones);
+
+			if('connections' in $scope.nginx){
+
+
+				d.connections.reqs = $scope.prSecond(d.connections.total, $scope.nginx.connections.total);
+
+				d.traffic.outBytes_S = $scope.prSecond(d.traffic.outBytes, $scope.nginx.traffic.outBytes);
+				d.traffic.inBytes_S = $scope.prSecond(d.traffic.inBytes, $scope.nginx.traffic.inBytes);
+
+				if($scope.mainTrafficGraph.length >= 90){
+					$scope.mainTrafficGraph.splice(0,1);
+				}
+
+				if($scope.mainGraph.length >= 90){
+					$scope.mainGraph.splice(0,1);
+				}
+
+				$scope.mainGraph.push({
+					x : now,
+					"Requests": d.connections.reqs,
+					"2xx" : $scope.prSecond(d.TotalErrorCodes['2xx'], $scope.nginx.TotalErrorCodes['2xx']),
+					"3xx" : $scope.prSecond(d.TotalErrorCodes['3xx'], $scope.nginx.TotalErrorCodes['3xx']),
+					"4xx" : $scope.prSecond(d.TotalErrorCodes['4xx'], $scope.nginx.TotalErrorCodes['4xx']),
+					"5xx" : $scope.prSecond(d.TotalErrorCodes['5xx'], $scope.nginx.TotalErrorCodes['5xx'])
+				});
+				$scope.mainTrafficGraph.push({
+						x : now,
+						"Sent" : (d.traffic.outBytes_S / 1024),
+						"Received" : (d.traffic.inBytes_S / 1024)
+				});
+			}
+
+
+
+			$log.info(d);
 			$scope.nginx= d;
 
 		});
-
-		//$interval.cancel($scope.start);
 	},1000)
 
 	$scope.getHostState = function (obj){
@@ -100,24 +155,68 @@ app.controller('main', [ "$scope", "$interval", "req", function ($scope, $interv
 		return arr;
 	};
 
+	$scope.getHits = function (obj){
+		return obj.hit + obj.revalidated;
+	};
+	$scope.getMiss = function (obj){
+		return obj.bypass + obj.expired + obj.scarce + obj.updating;
+	};
+
 	$scope.prSecond = function (a,b){
 		return a-b;
 	};
 
-	$scope.hitRatio=function (obj){
-		var key, hits = 0, miss=0;
-		for ( key in obj){
-			if(key == 'total'){
-				continue;
-			}
-			if (key == 'hit' || key == 'stale' || key == 'revalidated'){
-				hits += obj[key];
-			}else{
-				miss += obj[key];
-			}
+	$scope.aggrigateTraffic = function (obj){
+		var key, zone;
+		var output = {
+			inBytes : 0,
+			outBytes : 0
+		};
+
+		for (zone in obj){
+			output.inBytes+=obj[zone].inBytes;
+			output.outBytes+=obj[zone].outBytes;
 		}
 
-		return (hits/obj.total) * 100;
+		return output;
+	};
+	$scope.aggrigateErrorCodes = function (obj){
+		var key, zone, output = {};
+		var reg = new RegExp('[0-9]xx');
+		for (zone in obj){
+			for (key in obj[zone].responses){
+				if(reg.test(key)){
+					if(key in output){
+						output[key] += obj[zone].responses[key];
+					}else{
+						output[key] = obj[zone].responses[key];
+					}
+				}
+			}
+		}
+		return output;
+	};
+
+	$scope.RegexCount = function(obj, regex){
+		var key, total = 0, reg = new RegExp(regex);
+		for (key in obj){
+			if (reg.test(key)){
+				total += obj[key];
+			}
+		}
+		return total;
+	};
+
+	$scope.hitRatio=function (obj){
+		var key, hits = 0, miss=0;
+		if(!('total' in obj)){
+			obj.total = $scope.getTotal(obj, 'cache');
+		}
+
+		miss = obj.bypass + obj.expired + obj.scarce + obj.updating;
+		hits = obj.hit + obj.revalidated;
+		var ratio = (hits/obj.total) * 100;
+		return (isNaN(ratio)) ? 0 : ratio;
 	};
 
 	$scope.getTotal = function (obj, type){
@@ -134,14 +233,9 @@ app.controller('main', [ "$scope", "$interval", "req", function ($scope, $interv
 					total +=obj[key];
 				}
 			}
-
-
 		}
-
 		return total;
 	};
-
-
 }]);
 
 
